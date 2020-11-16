dff5ebd1b360c20f5d8c2b722b080da8f5a5c7a5
dambya <qwerty6789011@gmail.com>
2020-11-17 00:30:43 +0900

    섹션 8~10 정리 완료

---
+++ dambi/code/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 
+      구 컴포넌트 명명법 
+      <hello-world></hello-world> 
+    -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    // 'hello-world': HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
