283094c459c390a87d3624e3c158a43a2d6ee3ff
imyoly <67236343+imyoly@users.noreply.github.com>
2020-11-18 23:26:53 +0900

    ✍section 10 code commit

---
+++ myohy/code/vue-cli/src/a.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png" alt="Vue logo">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <!-- 컴포넌트 명명법 종류 -->
+        <!-- <hello-World></hello-World>
+        <HelloWorld></HelloWorld>
+        <HelloWorld/> -->
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+    // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
+    name: 'app',
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
