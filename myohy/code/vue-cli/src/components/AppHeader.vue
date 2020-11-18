fdd89dad02473f378154771bdc564691a4a9930f
imyoly <67236343+imyoly@users.noreply.github.com>
2020-11-19 00:46:44 +0900

    ✍section 11 code commit

---
+++ myohy/code/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>Header</h1>
+        <span>{{ propsdata }}</span>
+        <button v-on:click="sendEvent">send</button>
+    </header>
+</template>
+
+<script>
+export default {
+    props: [ 'propsdata' ],
+    methods: {
+        sendEvent: function () {
+            this.$emit('renew'); // 위로 이벤트 전달
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
